21a0f40548ef82c3228fd0363ca0c9dbb26b17cb
jereor <jere.orava@hotmail.com>
2026-05-30 15:51:03 +0300

    Add Unreal project with a small graybox hallway with one door.

---
+++ Source/TacticalDoorBreach/TacticalDoorBreachCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "TacticalDoorBreachCameraManager.h"
+
+ATacticalDoorBreachCameraManager::ATacticalDoorBreachCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
